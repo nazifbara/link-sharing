2ccb2aaa1d84b729fc4b56ec23bff54e7b5b07b1
nazifbara <nazifbarassounon@gmail.com>
2023-08-08 18:49:09 +0100

    create custom classes for typography

---
+++ src/routes/typography/+page.svelte
@@ -0,0 +1,14 @@
+<div class="grid gap-10 p-10">
+	<h1 class="heading-m">Heading M</h1>
+	<h2 class="heading-s">Heading S</h2>
+	<h1 class="body-m">
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus doloribus praesentium
+		earum dolor possimus tempora dignissimos error enim vel assumenda dolores ex voluptatibus,
+		deleniti perspiciatis labore, consectetur harum nulla! Odit.
+	</h1>
+	<h1 class="body-s">
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, perferendis culpa architecto
+		harum velit consectetur adipisci, magni quod pariatur, quaerat vel est ipsam totam distinctio
+		repellendus non nulla natus eaque?
+	</h1>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      base: ['Instrument Sans', 'sans-serif']
+    },
+    colors: {
+      contrast: {
+        base: '#737373',
+        dark: '#333333',
+      }
+    },
     extend: {},
   },
   plugins: [],
