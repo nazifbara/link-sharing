3491e3bf17a1371446fd47c9227552ecf1b3a1ff
nazifbara <nazifbarassounon@gmail.com>
2023-08-15 16:13:30 +0100

    add custom classes for the buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    fontFamily: {
-      base: ['Instrument Sans', 'sans-serif']
-    },
-    colors: {
-      primary: {
-        base: '#633CFF',
-        hover: '#BEADFF',
-        light: '#EFEBFF',
-      },
-      danger: '#FF3939',
-      contrast: {
-        base: '#737373',
-        dark: '#333333',
-      },
-      border: '#D9D9D9',
-      surface: '#FFFFFF',
-    },
-    extend: {
-      boxShadow: {
-        base: '0 0 32px rgba(0, 0, 0, 0.1)',
-      },
-      dropShadow: {
-        primary: '0 0 32px rgba(99, 60, 225, 0.25)',
-      }
-    },
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		fontFamily: {
+			base: ['Instrument Sans', 'sans-serif']
+		},
+		extend: {
+			colors: {
+				primary: {
+					base: '#633CFF',
+					hover: '#BEADFF',
+					light: '#EFEBFF'
+				},
+				danger: '#FF3939',
+				contrast: {
+					base: '#737373',
+					dark: '#333333'
+				},
+				border: '#D9D9D9',
+				surface: '#FFFFFF'
+			},
+			boxShadow: {
+				base: '0 0 32px rgba(0, 0, 0, 0.1)'
+			},
+			dropShadow: {
+				primary: '0 0 32px rgba(99, 60, 225, 0.25)'
+			}
+		}
+	},
+	plugins: []
+};
